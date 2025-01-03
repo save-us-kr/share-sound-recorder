5443c4c9ed914218ef6bbb5ccb061fbb48362c02
SANGWOO PARK <prophet0915@gmail.com>
2025-01-03 12:42:30 +0900

    prepare the UI needed for test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,125 +1,161 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_logger_plus/flutter_logger_plus.dart';
+import 'package:flutter_sound/flutter_sound.dart' as sound;
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: Audio()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Audio extends StatefulWidget {
+  const Audio({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<StatefulWidget> createState() => _AudioState();
+}
+
+class _AudioState extends State<Audio> {
+  @override
+  void initState() {
+    super.initState();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void dispose() {
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String formatTime(Duration duration) {
+    logger.info("formatTime duration: $duration");
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    int minutes = duration.inMinutes.remainder(60);
+    int seconds = duration.inSeconds.remainder(60);
+
+    String result = '$minutes:${seconds.toString().padLeft(2, '0')}';
+
+    logger.info("formatTime result: $result");
+    return result;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        centerTitle: true,
+        elevation: 0.0,
+        title: const Text(
+          '녹음',
+          style: TextStyle(color: Colors.black),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(40, 0, 40, 0),
+            child: Column(
+              children: [
+                SliderTheme(
+                  data: const SliderThemeData(
+                    inactiveTrackColor: Colors.grey,
+                  ),
+                  child: Slider(
+                    min: 0,
+                    max: duration.inSeconds.toDouble(),
+                    value: position.inSeconds.toDouble(),
+                    onChanged: (value) async {
+                      setState(() {
+                        position = Duration(seconds: value.toInt());
+                      });
+                      await audioPlayer.seek(position);
+                    },
+                    activeColor: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        formatTime(position),
+                        style: const TextStyle(color: Colors.brown),
+                      ),
+                      const SizedBox(width: 20),
+                      CircleAvatar(
+                        radius: 15,
+                        backgroundColor: Colors.transparent,
+                        child: IconButton(
+                          padding: const EdgeInsets.only(bottom: 50),
+                          icon: Icon(
+                            isPlaying ? Icons.pause : Icons.play_arrow,
+                            color: Colors.brown,
+                          ),
+                          iconSize: 25,
+                          onPressed: () async {
+                            logger.info("isplaying 전 : $isPlaying");
+
+                            if (isPlaying) {
+                              //재생중이면
+                              await audioPlayer.pause(); //멈춤고
+                              setState(() {
+                                isPlaying = false; //상태변경하기..?
+                              });
+                            } else {
+                              //멈춘 상태였으면
+                              //  await playAudio();
+                              await audioPlayer.resume(); // 녹음된 오디오 재생
+                            }
+                            logger.info("isplaying 후 : $isPlaying");
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Text(
+                        formatTime(duration),
+                        style: const TextStyle(color: Colors.brown),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          SizedBox(
+            child: IconButton(
+              onPressed: () async {
+                if (recorder.isRecording) {
+                  // await stop();
+                } else {
+                  // await record();
+                }
+                setState(() {});
+              },
+              icon: Icon(
+                recorder.isRecording ? Icons.stop : Icons.mic,
+                size: 30,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  bool isPlaying = false;
+  bool isRecording = false;
+
+  String audioPath = '';
+  String playAudioPath = '';
+
+  Duration duration = Duration.zero;
+  Duration position = Duration.zero;
+
+  final audioPlayer = AudioPlayer();
+  final recorder = sound.FlutterSoundRecorder();
 }
